7abc189f80b8db97c3804b3ebb772b1817299870
Victoriya Gorskaya <victoriya.gorskaya@mail.ru>
2022-12-22 13:17:43 +0300

    Initial commit

---
+++ application/db/people.py
@@ -0,0 +1,5 @@
+def get_employees():
+    print('get_employees')
+    
+if __name__ == '__main__':
+    get_employees()
---
+++ main.py
@@ -0,0 +1,20 @@
+from application.db.people import get_employees
+from application.salary import calculate_salary
+
+from datetime import datetime, time, date
+print(datetime.now())
+
+# from prettytable import PrettyTable
+# table = PrettyTable(["животное", "свирепость"])
+# table.add_row(["Оборотень", 100])
+# table.add_row(["Гризли", 87])
+# table.add_row(["Кролик из Кэрбенног", 110])
+# table.add_row(["Кот", -1])
+# table.add_row(["Утконос", 23])
+# table.add_row(["Дельфин", 63])
+# table.add_row(["Альбатрос", 44])
+# table.sort_key("свирепость")
+# table.reversesort = True
+# print(table)
+
+       
